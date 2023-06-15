82fef1be1e5fbd553e64f03d4fd09e6d7b7b27fc
Людмила Алтухова <luda.tamb@mail.ru>
2023-06-15 13:15:59 +0300

    refactor: correct server

--- server/server.js
+++ server/server.js
@@ -2,10 +2,6 @@ const http = require("http");
 
 const { findUsers } = require("./services/findUsers");
 
-// app.get("/api", (req, res) => {
-//   res.json({ message: "Hello from server!" });
-// });
-
 const server = http.createServer(function (request, response) {
   if (request.url === "/api") {
     let chunks = [];
@@ -14,34 +10,19 @@ const server = http.createServer(function (request, response) {
     });
 
     request.on("end", () => {
-      const data = Buffer.concat(chunks);
-      const querystring = data.toString();
-      const resivedData = JSON.parse(querystring);
-      const searchResult = findUsers(resivedData);
-      //////////
-      // const html = `
-      //       <div>
-      //       ${searchResult}
-      //       </div>`;
-      // response.writeHead(200, { "Content-Type": "text/html" });
-      response.write(JSON.stringify(searchResult));
-      response.end();
-      // response.write("<h2>hello world</h2>");
-      // response.end();
+      setTimeout(() => {
+        const data = Buffer.concat(chunks);
+        const querystring = data.toString();
+        const resivedData = JSON.parse(querystring);
+        const searchResult = findUsers(resivedData);
+        response.writeHead(200, { "Content-Type": "application/json" });
+        response.write(JSON.stringify(searchResult));
+        response.end();
+      }, 5000);
     });
-
-    //   response.write("<h2>hello world</h2>");
-
-    //response.end();
   }
 });
-
-//const port = 8000;
 
 const PORT = process.env.PORT || 3001;
 server.listen(PORT);
 console.log(`Listening at :${PORT}`);
-
-// const host = "127.0.0.1";
-// server.listen(PORT, host);
-// console.log(`Listening at http://${host}:${PORT}`);
